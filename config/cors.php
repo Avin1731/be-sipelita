2be86f2aff825489dc49e4905cbc2f72dfdc3601
Hilarius Christiano Avin Paliling <82177525+Avin1731@users.noreply.github.com>
2025-11-24 20:34:47 +0700

    Add additional allowed origins for CORS
    serangga

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://pad-git-main-avins-projects-8786affe.vercel.app',
+        'https://pad-mtr9i6bvw-avins-projects-8786affe.vercel.app',
+        'https://web-production-65a22.up.railway.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
